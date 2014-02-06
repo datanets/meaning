9dea4abe35965691cedae97b5a90293f86e26a3b
datanets <Andrew.Summers@oit.edu>
2014-02-05 20:27:13 -0800

    test updates

--- tests.js
+++ tests.js
@@ -1,10 +1,17 @@
-test( "learn one meaning", function() {
+test( "test learn one meaning", function() {
   result = parse( "x is y" );
   equal( result, true, "passes because x now means y" );
 });
 
-test( "return one meaning", function() {
+test( "test return one meaning", function() {
   result = parse( "x is y" );
   result = parse( "what is x?" );
   equal( result, "y", "passes because meaning of x is returned" );
+});
+
+test( "test meaning equality", function() {
+  result = parse( "x is a" );
+  result = parse( "y is a" );
+  result = parse( "is x y?" );
+  equal( result, true, "passes because meanings are equal" );
 });
